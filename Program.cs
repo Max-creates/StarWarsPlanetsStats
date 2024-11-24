c45fb1f53f80b3842c07bb686b605e2e39405c08
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2024-11-24 15:21:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using StarWarsPlanetsStats.ApiDataAccess;
+using StarWarsPlanetsStats.DTOs;
+using System.Text.Json;
+
+try
+{
+    await new StarWarsPlanetsStatsApp(
+    new ApiDataReader(),
+    new MockStarWarsApiDataReader()).Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred. " +
+        "Exception message: " + ex.Message);
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+public class StarWarsPlanetsStatsApp
+{
+    private readonly IApiDataReader _apiDataReader;
+    private readonly IApiDataReader _secondaryApiDataReader;
+
+    public StarWarsPlanetsStatsApp(IApiDataReader apiDataReader, IApiDataReader secondaryApiDataReader)
+    {
+        _apiDataReader = apiDataReader;
+        _secondaryApiDataReader = secondaryApiDataReader;
+    }
+
+
+    public async Task Run()
+    {
+
+        string? json = null;
+        try
+        {
+            json = await _apiDataReader.Read(
+                "https://swapi.dev/", "api/planets");
+
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("API request was unsuccessful. " +
+                "Switching to mock data. " +
+                "Exception message: " + ex.Message);
+        }
+
+        if (json != null)
+        {
+            json = await _secondaryApiDataReader.Read(
+                "https://swapi.dev/", "api/planets");
+        }
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+
+        var planets = ToPlanets(root);
+    }
+
+    private IEnumerable<Planet> ToPlanets(Root? root)
+    {
+        if (root == null)
+        {
+            throw new ArgumentNullException(nameof(root));
+        }
+        throw new NotImplementedException();
+    }
+}
+
+public readonly record struct Planet
+{
+    public string Name { get; }
+    public int Diameter { get; }
+    public int? SurfaceWater { get; }
+    public int? Population { get; }
+
+    public Planet(
+        string name,
+        int diameter,
+        int? surfaceWater,
+        int? population)
+    {
+        if (name is null)
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+        Name = name;
+        Diameter = diameter;
+        SurfaceWater = surfaceWater;
+        Population = population;
+    }
+}
